d1b2af0d698a3c5d0623d09630dc7c6ddb93783b
Nargiza <nargizok04s@gmail.com>
2023-04-30 16:54:45 +0500

    documentation of queue

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -2,18 +2,38 @@ import java.util.NoSuchElementException;
 
 public class MyLinkedListQueue<E> {
     private MyLinkedList<E> queue;
-    public MyLinkedListQueue(){
+    public MyLinkedListQueue(){ //constructor
         queue = new MyLinkedList<>();
     }
+    // method enqueue
+    /**
+     * add the element to the queue
+     * @param element the element that will add in queue
+     */
+
     public void enqueue(E element){
         queue.add(element, size()-1);
     }
+    // method dequeue
+
+    /**
+     * remove the first elements of queue and return it
+     * @return the first elements of the queue
+     * @throws if the queue is empty
+     */
     public E dequeue(){
         if (isEmpty()){
             throw new NoSuchElementException();
         }
         return queue.removed(0);
     }
+    //method peek
+
+    /**
+     * return the first elements of queue
+     * @return the first elements of the queue
+     * @throws if the queue is empty
+     */
     public E peek(){
         if (isEmpty()){
             throw new NoSuchElementException();
@@ -22,6 +42,12 @@ public class MyLinkedListQueue<E> {
             return queue.get(0);
         }
     }
+    // method isEmpty
+
+    /**
+     * if queue is empty return true, if not false
+     * @return true or false
+     */
     public boolean isEmpty(){
         if (queue.size() == 0){
             return true;
@@ -30,6 +56,12 @@ public class MyLinkedListQueue<E> {
             return false;
         }
     }
+    //method size
+
+    /**
+     * return size or the number of elements in queue
+     * @return size
+     */
     public int size() {
         return queue.size();
     }
