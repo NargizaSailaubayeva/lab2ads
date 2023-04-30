3dffc2adf9e00a798b5954c0a8f09d591ad7793f
Nargiza <nargizok04s@gmail.com>
2023-04-30 15:55:04 +0500

    method isEmpty

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -8,6 +8,14 @@ public class MyLinkedListQueue<E> {
     public void enqueue(E element){
         queue.add(element, size()-1);
     }
+    public boolean isEmpty(){
+        if (queue.size() == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public int size() {
         return queue.size();
     }
