0b0b69fe5f0cd91b269b5f80cf429bf25cebdc99
Nargiza <nargizok04s@gmail.com>
2023-05-01 14:00:23 +0500

    method enqueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,7 +1,11 @@
 import java.util.NoSuchElementException;
 public class MyArrayListQueue<E> {
     private MyArrayList<E> queue;
+    public MyArrayListQueue() {
+        queue = new MyArrayList<>();
+    }
     public void enqueue(E element) {
         queue.add(element);
     }
+
 }
