cc66a94cad3c57d8d902ad65f87c79fc634bc98f
Nargiza <nargizok04s@gmail.com>
2023-05-01 13:51:52 +0500

    MyArrayListQueue add constructors

--- MyArrayListQueue.java
+++ MyArrayListQueue.java
@@ -1,4 +1,8 @@
 import java.util.NoSuchElementException;
 public class MyArrayListQueue<E> {
     private MyArrayList<E> queue;
+    public MyArrayListQueue() { //constructor
+        queue = new MyArrayList<>();
+    }
+
 }
